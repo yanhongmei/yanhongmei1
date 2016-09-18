010ac5d82edfc9198388f4a2527e228dc9423979
DC004 <dc004@yanhongmei.lan>
2016-09-18 10:16:30 +0800

    Initial Commit

---
+++ zc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  zc
+//
+//  Created by DC004 on 16/9/18.
+//  Copyright © 2016年 闫红梅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ zc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  zc
+//
+//  Created by DC004 on 16/9/18.
+//  Copyright © 2016年 闫红梅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
